d5f4630e62315182cf85960286947dd96c3ad0f7
anchi204 <163340813+anchi204@users.noreply.github.com>
2026-08-22 04:47:53 +0530

    style: improve dashboard shell spacing and background

--- components/layout/dashboard-layout.tsx
+++ components/layout/dashboard-layout.tsx
@@ -1,20 +1,6 @@
 import Sidebar from "./sidebar";
 import Navbar from "./navbar";
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="flex">
-      <Sidebar />
-
-      <main className="flex-1">
-        <Navbar />
-
-        <div className="p-8">{children}</div>
-      </main>
-    </div>
-  );
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <div className="min-h-screen bg-muted/20"><div className="flex min-h-screen"><Sidebar /><main className="min-w-0 flex-1"><Navbar /><div className="p-4 sm:p-6 lg:p-8">{children}</div></main></div></div>;
 }
